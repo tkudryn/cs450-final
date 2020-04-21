0800fb53b416fc0d2f659370335e13c522ba32cc
emilyrisley <emilypinkypie@gmail.com>
2020-04-20 18:52:39 -1000

    updated statistics page for date field, drop down menu, and updated SQL query

--- python/gui-database.py
+++ python/gui-database.py
@@ -225,15 +225,36 @@ class StatisticsPage(tk.Frame):
 
         # Assign entry box values to variables
         self.STATE = self.Entry1.get()
-        self.TYPE = self.Entry2.get()
+        self.DATE = self.Entry2.get()
+        self.TYPE = self.Entry3Choice.get()
+
+        #COLUMN is what goes in select statement
+        self.COLUMN = self.TYPE
 
         # Output for testing, remove before submission
         print(self.STATE)
         print(self.TYPE)
 
+        #create syntax for rate column
+        if self.TYPE == 'rate of death':
+            self.COLUMN = '(deaths/cases)'
+            self.TYPE = 'rate'
+
+        # create syntax for state conditional (if all states, do not specify)
+        if self.STATE == "All":
+            self.STATE = ''
+
+            #syntax for SUM is different for rate than cases or deaths
+            if (self.TYPE == 'rate'):
+                self.COLUMN = 'SUM(deaths)/SUM(cases)'
+            else:
+                self.COLUMN = 'SUM(' + self.COLUMN + ')'
+        else:
+            self.STATE = "AND state = '" + self.STATE + "'"
+
         # reset text to empty in case this is a subsequent call of QUERY
         self.Scrolledtext2.delete(1.0, "end")
-        # Assign vairables to mysql statements and execute
+        # Assign variables to mysql statements and execute
         # Open MySQL connection
         self.cnx = mysql.connector.connect(user='project', password=credentials.password, database='cs450', host=credentials.host)
         self.cur = self.cnx.cursor()
@@ -241,7 +262,7 @@ class StatisticsPage(tk.Frame):
 
         # need to figure out how to do "all" for state
         self.query1 = (
-            "SELECT {} FROM {} WHERE state = '{}' ORDER BY {} DESC".format(self.TYPE, "project", self.STATE, self.TYPE))
+            "SELECT {} FROM {} WHERE date = '{}' {}".format(self.COLUMN, "project", self.DATE, self.STATE, self.TYPE))
         print(self.query1)
         self.cur.execute(self.query1)
         print("Simple Query")
@@ -271,22 +292,35 @@ class StatisticsPage(tk.Frame):
         self.Label1.configure(text='''State''')
 
         self.Label2 = tk.Label(self)
-        self.Label2.place(relx=0.183, rely=0.2, height=18, width=46)
+        self.Label2.place(relx=0.183, rely=0.2, height=28, width=46)
         self.Label2.configure(activebackground="#f9f9f9")
-        self.Label2.configure(text='''Type''')
+        self.Label2.configure(text='''Date''')
+
+        self.Label3 = tk.Label(self)
+        self.Label3.place(relx=0.183, rely=0.311, height=18, width=46)
+        self.Label3.configure(activebackground="#f9f9f9")
+        self.Label3.configure(text='''Type''')
 
         # Select input box
         self.Entry1 = tk.Entry(self)
-        self.Entry1.place(relx=0.317, rely=0.089, height=20, relwidth=0.243)
+        self.Entry1.place(relx=0.4, rely=0.089, height=20, relwidth=0.4)
         self.Entry1.configure(background="white")
         self.Entry1.configure(font="TkFixedFont")
         self.Entry1.configure(selectbackground="#c4c4c4")
-        # FROM input box
+
+        # Select input box
         self.Entry2 = tk.Entry(self)
-        self.Entry2.place(relx=0.317, rely=0.2, height=20, relwidth=0.243)
+        self.Entry2.place(relx=0.4, rely=0.2, height=20, relwidth=0.4)
         self.Entry2.configure(background="white")
         self.Entry2.configure(font="TkFixedFont")
         self.Entry2.configure(selectbackground="#c4c4c4")
+
+        # type input box
+        self.Entry3Choice = tk.StringVar()
+        self.Entry3Choice.set("cases")  # default value
+        self.Entry3 = tk.OptionMenu(self, self.Entry3Choice, "cases", "deaths", "rate of death")
+        self.Entry3.place(relx=0.4, rely=0.311, height=30, relwidth=0.4)
+
         # Query OutPut box
         self.Scrolledtext2 = scrolledtext.ScrolledText(self)
         self.Scrolledtext2.place(relx=0.0, rely=0.667, relheight=0.262
